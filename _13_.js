713b8dcc259ab28a8fc9404fed71b2c06d894c47
Shashi Singh <shashisingh92292@gmail.com>
2024-11-19 15:19:06 +0530

    pranktry1

--- _13_.js
+++ _13_.js
@@ -13,35 +13,35 @@ function randomjokefull(innerText){
     return (`${joke.setup} - ${joke.punchline}`);
 }
 
-// let a = document.getElementsByTagName("div")
+let a = document.getElementsByTagName("div")
 
-// a[1].innerText = randomjokesetup()
-// a[2].innerText = randomjokesetup()
-// a[3].innerText = randomjokesetup()
-// a[4].innerText = randomjokesetup()
-// a[5].innerText = randomjokesetup()
-// a[6].innerText = randomjokesetup()
-// a[7].innerText = randomjokesetup()
+a[1].innerText = randomjokesetup()
+a[2].innerText = randomjokesetup()
+a[3].innerText = randomjokesetup()
+a[4].innerText = randomjokesetup()
+a[5].innerText = randomjokesetup()
+a[6].innerText = randomjokesetup()
+a[7].innerText = randomjokesetup()
 
-// a[1].addEventListener('click', () => {
-//     a[1].innerText = randomjokefull(a[1].innerText)
-// });
+a[1].addEventListener('click', () => {
+    a[1].innerText = randomjokefull(a[1].innerText)
+});
 
-// a[2].addEventListener('click', () => {
-//     a[2].innerText = randomjokefull(a[2].innerText)
-// });
-// a[3].addEventListener('click', () => {
-//     a[3].innerText = randomjokefull(a[3].innerText)
-// });
-// a[4].addEventListener('click', () => {
-//     a[4].innerText = randomjokefull(a[4].innerText)
-// });
-// a[5].addEventListener('click', () => {
-//     a[5].innerText = randomjokefull(a[5].innerText)
-// });
-// a[6].addEventListener('click', () => {
-//     a[6].innerText = randomjokefull(a[6].innerText)
-// });
-// a[7].addEventListener('click', () => {
-//     a[7].innerText = randomjokefull(a[7].innerText)
-// });
+a[2].addEventListener('click', () => {
+    a[2].innerText = randomjokefull(a[2].innerText)
+});
+a[3].addEventListener('click', () => {
+    a[3].innerText = randomjokefull(a[3].innerText)
+});
+a[4].addEventListener('click', () => {
+    a[4].innerText = randomjokefull(a[4].innerText)
+});
+a[5].addEventListener('click', () => {
+    a[5].innerText = randomjokefull(a[5].innerText)
+});
+a[6].addEventListener('click', () => {
+    a[6].innerText = randomjokefull(a[6].innerText)
+});
+a[7].addEventListener('click', () => {
+    a[7].innerText = randomjokefull(a[7].innerText)
+});
